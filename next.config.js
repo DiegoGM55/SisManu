365bd66438b06af9ae66115505503a6b7a65161e
DiegoGM55 <diego.gmota07@gmail.com>
2023-12-11 14:56:51 -0300

    next config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ['images.unsplash.com']
-  }
+  reactStrictMode: true
 };
 
-export default nextConfig;
+module.exports = nextConfig;
